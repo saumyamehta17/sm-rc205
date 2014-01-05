e3c33d4050850f1b2ae21cc82e7bbdcada59a687
sweetymehta <smehta1289@gmail.com>
2014-01-05 15:58:00 +0530

    javascript without inline

---
+++ README.md
@@ -0,0 +1,22 @@
+Railscast sm-rc205
+====================
+Git clone
+```
+git clone 'https://github.com/sweetymehta/sm-rc205.git'
+```
+
+csrf_meta_tag
+```
+cross site request forgery(imitation of doc) ,these element prevents cross site request forgery when users enter form input.
+when a user sign in, session may be maintained for that site for that user. Another attacker can use forgery to  access that site.
+```
+See diff of abtrosive and unabtrosive javascript
+```
+see articles/index.html.erb
+```
+Rails server
+```
+rails s
+```
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SmRc205::Application.config.secret_key_base = 'a791af5bd072d3ed125666e237e578ed95bc74f95362b91a70a06ad3d470874e7f3e0fc0aa2e83cc7ab69a0054993fbfcdaf36aefc82980c3fa0564d4eb6c3dc'
